248bac33bed045728a127c65d4070ec581c8073a
sacha.lasry <sacha.lasry@artefact.com>
2021-01-11 19:54:52 +0100

    added other languages in GCS

--- trainer/fastai_config.py
+++ trainer/fastai_config.py
@@ -1,12 +1,34 @@
 import os
+## Model customization ##
+
+# Information on the DataFrame
+TEXT_COL_NAME = "text_clean"
+LABEL_COL_NAME = "labels"
+LABEL_DELIM = ","
+LABEL_LIST = ["skincare", "makeup"]
+OTHER_LABEL_NAME = "other"
+
+# Model parameters
+METRIC_TO_MONITOR = "valid_loss"
+DROP_MULT = 0.3 # Between 0 and 1
+LANGUAGE = 'fr' # Choose among ['en', 'fr', 'ko', 'ja', 'zh']
+BACKWARD = True 
+
+# Variables of train/test split
+RANDOM_STATE = 42
+VAL_SIZE = 0.2
+TEST_SIZE = 0.25
+
 
 ## Filepaths and filenames ##
 
 # GCS File paths
-DATASET_GCS_PATH = "pretrained/labelled_dataset_10k.csv"
-VOCAB_GCS_PATH = "pretrained/vocab.pkl"
-CONFIG_GCS_PATH = "pretrained/config.json"
-WEIGHTS_GCS_PATH = "pretrained/weights.pth"
+PRETRAINED_GCS_FOLDER = os.ath.join("pretrained", LANGUAGE)
+LM_TYPE_GCS_FOLDER = {False: 'forward', True: 'backward'}
+DATASET_GCS_PATH = os.path.join(PRETRAINED_GCS_FOLDER, "labelled_dataset.csv")
+VOCAB_GCS_PATH = os.path.join(PRETRAINED_GCS_FOLDER, LM_TYPE_GCS_FOLDER.get(BACKWARD), "vocab.pkl")
+CONFIG_GCS_PATH = os.path.join(PRETRAINED_GCS_FOLDER, LM_TYPE_GCS_FOLDER.get(BACKWARD), "config.json")
+WEIGHTS_GCS_PATH = os.path.join(PRETRAINED_GCS_FOLDER, LM_TYPE_GCS_FOLDER.get(BACKWARD), "weights.pth")
 
 # Local File paths
 MODELS_LOCAL_FOLDER = "models"
@@ -25,28 +47,3 @@ LM_MODEL_PATH = "."
 BESTMODEL_NAME = "bestmodel"
 ENCODER_FILE_NAME = "encoder"
 MODEL_FILE_NAME = "fastai_model.pth"
-
-
-## Model customization ##
-
-# Information on the DataFrame
-TEXT_COL_NAME = "text_clean"
-LABEL_COL_NAME = "labels"
-LABEL_DELIM = ","
-LABEL_LIST = ["skincare", "makeup"]
-OTHER_LABEL_NAME = "other"
-
-# Model parameters
-METRIC_TO_MONITOR = "valid_loss"
-DROP_MULT = 0.3
-LANGUAGE = 'fr'
-
-# Variables of train/test split
-RANDOM_STATE = 42
-VAL_SIZE = 0.2
-TEST_SIZE = 0.25
-
-
-
-
-
